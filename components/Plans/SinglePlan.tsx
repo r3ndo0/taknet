9a3eacdc4a0e050b33ced3c1423a8496904f41b8
r3ndo0 <r3ndo0@gmail.com>
2024-08-17 01:22:16 -0700

    my first commit

---
+++ components/Plans/SinglePlan.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SinglePlan() {
+  return <div>SinglePlan</div>;
+}
+
+export default SinglePlan;
---
+++ components/Plans/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Plans() {
+  return <div>Plans</div>;
+}
+
+export default Plans;
